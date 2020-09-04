a46e4664f68aaec71f1a1e3a5636ad73d47c8dd6
陈飞豪 <chenfeihao1@gmail.com>
2020-09-04 19:47:58 +0800

    @misc

--- xhaka_client/src/main/java/com/tinysakura/xhaka/client/slave/bootstrap/XhakaSlaveBootstrap.java
+++ xhaka_client/src/main/java/com/tinysakura/xhaka/client/slave/bootstrap/XhakaSlaveBootstrap.java
@@ -1,5 +1,6 @@
 package com.tinysakura.xhaka.client.slave.bootstrap;
 
+import com.tinysakura.xhaka.common.context.XhakaWebServerContext;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.ApplicationArguments;
@@ -7,19 +8,29 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.util.Objects;
+
 /**
  * @Author: chenfeihao@corp.netease.com
  * @Date: 2020/8/31
  */
 public class XhakaSlaveBootstrap implements ApplicationRunner, ApplicationContextAware {
-    private ApplicationContext applicationContext;
+    private ApplicationContext context;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String serverName = applicationContext.getEnvironment().getProperty("app_name");
+        String serverName = context.getEnvironment().getProperty("app_name");
         if (StringUtils.isEmpty(serverName)) {
-            serverName = applicationContext.getEnvironment().getProperty("spring.application.name");
+            serverName = context.getEnvironment().getProperty("spring.application.name");
         }
+
+        Integer port = Integer.valueOf(Objects.requireNonNull(context.getEnvironment().getProperty("server.port")));
+        String contextPath = context.getEnvironment().getProperty("server.servlet.context-path");
+        if (StringUtils.isEmpty(contextPath)) {
+            contextPath = "/";
+        }
+
+        new XhakaWebServerContext(contextPath, port);
 
         final String finalServerName = serverName;
         new Thread(() -> {
@@ -29,6 +40,6 @@ public class XhakaSlaveBootstrap implements ApplicationRunner, ApplicationContex
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+        this.context = applicationContext;
     }
 }
