092d039811b9652d1e16c6ed2fc77dea05f4e5ed
陈飞豪 <chenfeihao1@gmail.com>
2020-09-04 15:55:13 +0800

    @misc flush

--- xhaka_commons/src/main/java/com/tinysakura/xhaka/common/gateway/handler/XhakaProtocolHandler.java
+++ xhaka_commons/src/main/java/com/tinysakura/xhaka/common/gateway/handler/XhakaProtocolHandler.java
@@ -47,6 +47,7 @@ public class XhakaProtocolHandler extends SimpleChannelInboundHandler<Xhaka> {
             xhakaHeartRes.setPackType(XhakaHeaderConstant.XHAKA_PACK_TYPE_HEART);
             xhakaHeartRes.setEventType(XhakaHeaderConstant.XHAKA_EVENT_TYPE_REQUEST);
             ctx.pipeline().get(XhakaEncoder.class).write(ctx, xhakaHeartRes, ctx.voidPromise());
+            ctx.flush();
             return;
         }
 
