9d1a5b5493be8ccdc68946555aedf1d9b6a7b529
陈飞豪 <chenfeihao1@gmail.com>
2020-09-22 15:54:47 +0800

    @dev 加入对netty内存使用的监控

--- start_app/src/main/java/com/tinysakura/xhaka/XhakaApplication.java
+++ start_app/src/main/java/com/tinysakura/xhaka/XhakaApplication.java
@@ -3,8 +3,10 @@ package com.tinysakura.xhaka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(scanBasePackages = "com.tinysakura.xhaka")
+@ImportResource(locations = {"classpath:spring-mbean.xml"})
 public class XhakaApplication {
 
     public static void main(String[] args) {
