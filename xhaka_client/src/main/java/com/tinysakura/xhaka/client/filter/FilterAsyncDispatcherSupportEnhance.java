aa3500793fe0af00ed357803de3f234a08ec9ea8
陈飞豪 <chenfeihao1@gmail.com>
2020-10-14 16:16:53 +0800

    @misc

--- xhaka_client/src/main/java/com/tinysakura/xhaka/client/filter/FilterAsyncDispatcherSupportEnhance.java
+++ xhaka_client/src/main/java/com/tinysakura/xhaka/client/filter/FilterAsyncDispatcherSupportEnhance.java
@@ -1,5 +1,6 @@
 package com.tinysakura.xhaka.client.filter;
 
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.implementation.bind.annotation.Super;
 import org.apache.catalina.core.ApplicationFilterRegistration;
 
@@ -14,8 +15,12 @@ import java.util.EnumSet;
 public class FilterAsyncDispatcherSupportEnhance {
 
     public static void addMappingForServletNames(
-            EnumSet<DispatcherType> dispatcherTypes, boolean isMatchAfter,
-            String[] servletNames, @Super ApplicationFilterRegistration zuper) {
+            @AllArguments Object[] allParams, @Super ApplicationFilterRegistration zuper) {
+
+        EnumSet<DispatcherType> dispatcherTypes = (EnumSet<DispatcherType>) allParams[0];
+        boolean isMatchAfter = (boolean) allParams[1];
+        String[] servletNames = (String[]) allParams[2];
+
         if (!dispatcherTypes.contains(DispatcherType.ASYNC)) {
             dispatcherTypes.add(DispatcherType.ASYNC);
         }
