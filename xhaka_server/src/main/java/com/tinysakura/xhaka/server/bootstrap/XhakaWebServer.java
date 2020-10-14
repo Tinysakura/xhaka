19b082b127cc3a3f6594d443300494a204a588ae
陈飞豪 <chenfeihao1@gmail.com>
2020-10-14 20:59:07 +0800

    @fix 调整io线程数量

--- xhaka_server/src/main/java/com/tinysakura/xhaka/server/bootstrap/XhakaWebServer.java
+++ xhaka_server/src/main/java/com/tinysakura/xhaka/server/bootstrap/XhakaWebServer.java
@@ -48,7 +48,7 @@ public class XhakaWebServer implements WebServer {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         bossGroup = new NioEventLoopGroup(1);
         // 默认为cpu核数 * 2
-        workerGroup = new NioEventLoopGroup(20);
+        workerGroup = new NioEventLoopGroup();
         businessGroup = new DefaultEventLoopGroup(businessThreadCount);
 
         ChannelFuture future = serverBootstrap.channel(NioServerSocketChannel.class)
